b31f1f0c65cd351871e50f33083dda7bef09d608
xygorn <xygorn@users.noreply.github.com>
2014-08-29 13:04:13 -0400

    Update spritz.js

--- spritz.js
+++ spritz.js
@@ -80,7 +80,7 @@ function spritz(){
     }
 }
 function parse(input,width,height){
-     var f = Typeset.formatter(function (text) return text.length})
+     var f = Typeset.formatter(function (text) text.length;})
       var words = input.split(/\s+/);
       var id = 0;
       while (id<words.length){
